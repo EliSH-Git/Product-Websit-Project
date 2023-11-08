3c8cafb674cc7f7ab6a73ad3fc647ce0d85d497d
elaheh shabanian <elaheh.shabanian@gmail.com>
2023-11-08 14:50:32 +0100

    Add

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,18 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Header from './components/layout/Header';
-import Home from './components/Home';
- 
-
+import Header from '../components/layout/Header';
+import Home from '../components/Home';
 
 export const router = createBrowserRouter([
-    {
-        element: <Header/>,
-        children: [
-            {
-                path: '/',
-                element: <Home/>
-            }
-        ]
-
-    }
+  {
+    element: <Header />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
 ]);
+
+;
